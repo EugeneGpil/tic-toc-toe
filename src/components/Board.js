5d9841d85896c382bd40b390b52ce0589827deef
EugeneGpil <ev@freshcube.ru>
2021-06-10 09:08:14 +0300

    history

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,71 +1,22 @@
 import {useState} from 'react'
 import Square from 'components/Square'
 
-const Board = () => {
-  const [board, setBoard] = useState([
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-  ])
-  const [nextPlayer, setNextPlayer] = useState('X')
-  const [winner, setWinner] = useState(null)
-
-  const executeTurn = (x, y) => {
-    if (winner) {
-      return
-    }
-
-    const newBoard = board.map(row => [...row])
-    if (newBoard[x][y] === '') {
-      newBoard[x][y] = nextPlayer
-      setNextPlayer(nextPlayer === 'X' ? 'O' : 'X')
-    }
-
-    const lines = [
-      [[0, 0], [0, 1], [0, 2]],
-      [[1, 0], [1, 1], [1, 2]],
-      [[2, 0], [2, 1], [2, 2]],
-      [[0, 0], [1, 0], [2, 0]],
-      [[0, 1], [1, 1], [2, 1]],
-      [[0, 2], [1, 2], [2, 2]],
-      [[0, 0], [1, 1], [2, 2]],
-      [[0, 2], [1, 1], [2, 0]]
-    ]
-
-    let newWinner = null
-    for (const line of lines) {
-      const [[ax, ay], [bx, by], [cx, cy]] = line
-      if (newBoard[ax][ay] !== '' && newBoard[ax][ay] === newBoard[bx][by] && newBoard[ax][ay] === newBoard[cx][cy]) {
-        newWinner = newBoard[ax][ay]
-        break
-      }
-    }
-
-    if (newWinner !== null) {
-      setWinner(newWinner)
-    }
-
-    setBoard(newBoard)
-  }
+const Board = ({
+  board,
+  onClick
+}) => {
 
 
 
   const renderSquare = (x, y) => {
     return <Square
       value={board[x][y]}
-      onClick={() => {executeTurn(x, y)}}
+      onClick={() => {onClick(x, y)}}
     />
   }
 
   return (
     <div>
-      <div className="status">
-        {
-          winner
-            ? `${winner} win!`
-            : `Next player: ${nextPlayer}`
-        }
-      </div>
       <div className="board-row">
         {renderSquare(0, 0)}
         {renderSquare(0, 1)}
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,14 +1,114 @@
+import {useState} from 'react'
 import Board from 'components/Board'
 
 const Game = () => {
+
+  const [board, setBoard] = useState([
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ])
+
+  const [nextPlayer, setNextPlayer] = useState('X')
+  const [winner, setWinner] = useState(null)
+
+  const [history, setHistory] = useState([{
+    board,
+    nextPlayer
+  }])
+
+  const executeTurn = (x, y) => {
+    if (winner || board[x][y] !== '') {
+      return
+    }
+
+    const newBoard = board.map(row => [...row])
+    newBoard[x][y] = nextPlayer
+    const newNextPlayer = nextPlayer === 'X' ? 'O' : 'X'
+    setNextPlayer(newNextPlayer)
+
+    const lines = [
+      [[0, 0], [0, 1], [0, 2]],
+      [[1, 0], [1, 1], [1, 2]],
+      [[2, 0], [2, 1], [2, 2]],
+      [[0, 0], [1, 0], [2, 0]],
+      [[0, 1], [1, 1], [2, 1]],
+      [[0, 2], [1, 2], [2, 2]],
+      [[0, 0], [1, 1], [2, 2]],
+      [[0, 2], [1, 1], [2, 0]]
+    ]
+
+    let newWinner = null
+    for (const line of lines) {
+      const [[ax, ay], [bx, by], [cx, cy]] = line
+      if (newBoard[ax][ay] !== '' && newBoard[ax][ay] === newBoard[bx][by] && newBoard[ax][ay] === newBoard[cx][cy]) {
+        newWinner = newBoard[ax][ay]
+        break
+      }
+    }
+
+    if (newWinner !== null) {
+      setWinner(newWinner)
+    }
+
+    setBoard(newBoard)
+    setHistory([
+      ...history,
+      {
+        board: newBoard,
+        nextPlayer: newNextPlayer
+      }
+    ])
+  }
+
+  const goToStep = (step) => {
+    setHistory(history.slice(0, step + 1))
+    setBoard(history[step].board)
+    setNextPlayer(history[step].nextPlayer)
+  }
+
+  let status = `Next turn: ${nextPlayer}`
+  if (winner) {
+    status = `${winner} win!`
+  }
+
   return (
-    <div className="game">
-      <div className="game-board">
-        <Board />
+    <div className='game'>
+      <div className='game-board'>
+        <Board
+          board={board}
+          onClick={(x, y) => {executeTurn(x, y)}}
+        />
       </div>
-      <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
+      <div className='game-info'>
+        <div>{status}</div>
+        <div className='history'>
+          {history.map((step, stepKey) => {
+            return (
+              <div
+                key={stepKey}
+                className='history-step-board'
+                onClick={() => {goToStep(stepKey)}}
+              >
+                <div className='history-step-board-row'>
+                  <div className='history-step-cell'>{step.board[0][0]}</div>
+                  <div className='history-step-cell'>{step.board[0][1]}</div>
+                  <div className='history-step-cell'>{step.board[0][2]}</div>
+                </div>
+                <div className='history-step-board-row'>
+                  <div className='history-step-cell'>{step.board[1][0]}</div>
+                  <div className='history-step-cell'>{step.board[1][1]}</div>
+                  <div className='history-step-cell'>{step.board[1][2]}</div>
+                </div>
+                <div className='history-step-board-row'>
+                  <div className='history-step-cell'>{step.board[2][0]}</div>
+                  <div className='history-step-cell'>{step.board[2][1]}</div>
+                  <div className='history-step-cell'>{step.board[2][2]}</div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
